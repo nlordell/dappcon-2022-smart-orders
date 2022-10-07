fdd26e9d37eba96ebca315228f641f1f3d491e94
Felix Leupold <felixleupold90@gmail.com>
2022-10-07 21:34:38 +0200

    Add support for goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
 
-const {
-  INFURA_PROJECT_ID,
-  PRIVATE_KEY,
-} = process.env;
+const { INFURA_PROJECT_ID, PRIVATE_KEY } = process.env
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [PRIVATE_KEY]
-    }
-  }
-};
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+}
